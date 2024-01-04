80080f876fa5db2de80e1734eb25629241bf3791
aristrisnawan <aristrisnawan35@gmail.com>
2024-01-04 20:09:36 +0700

    add login function

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const jwt = require('jsonwebtoken')
 
 //SignUp
 router.post("/signup", async (req, res) => {
@@ -18,8 +19,22 @@ router.post("/signup", async (req, res) => {
 });
 
 //Login
-router.post("/login", () => {
-  console.log("Login");
+router.post("/login", async (req,res) => {
+  try {
+    const {username,password} = req.body
+    const user = await User.findOne({username})
+    if (!user) {
+      return res.status(401).json({message: 'Invalid Credential'})
+    }
+    const isPasswordValid = await user.comparePassword(password)
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" })
+    }
+    const token = jwt.sign({userId: user._id}, "youre-secret-key", {expiresIn: "1h"})
+    res.json({ token, message: "Login Success" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
